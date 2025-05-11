e554f2e64628835003ab33ed7258492c46b6aad0
Dima <d.chernushin@gmail.com>
2025-05-11 15:47:58 +0300

    task2 and 3

--- Work/main.py
+++ Work/main.py
@@ -1,8 +1,9 @@
 from sys import argv, exit
+from typing import Optional
 
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import pyqtSignal, pyqtBoundSignal, Qt
 from PyQt6.QtGui import QPixmap, QImage, QKeyEvent
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 from API_KEYS import *
 from Work.UI.Window import Ui_MainWindow
@@ -18,28 +19,54 @@ class SearchMapApp(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.button = QPushButton('Светлая/Тёмная', self)
+        self.button.move(530, 30)
+        bound: pyqtSignal | pyqtBoundSignal = self.button.clicked
+        bound.connect(self.set_theme)
+
+        self.current_theme = 'light'
+
         self.setFixedSize(650, 450)
         self.zoom = 17
+        self.current_pos = None
+        self.set_default_position()
         self.set_image()
 
-    def set_image(self):
+    def set_default_position(self):
         params = {
             'apikey': GEOCODE_API_KEY,
             'format': 'json',
             'geocode': 'Краснодар, Бульварное кольцо,  9'
         }
-        coords = get_coord_from_object(get_object(get_geocode_data(**params)))
-        coords = ','.join(map(str, coords))
+        self.current_pos = list(get_coord_from_object(get_object(get_geocode_data(**params))))
+
+    def set_theme(self):
+        self.current_theme = 'dark' if self.current_theme == 'light' else 'light'
+        self.set_image()
+
+    def set_image(self):
         params = {
             'apikey': STATIC_MAPS_API_KEY,
-            'll': coords,
+            'll': ','.join(map(str, self.current_pos)),
             'z': self.zoom,
-            'size': '650,450'
+            'size': '650,450',
+            'theme': self.current_theme
         }
         data = get_image_from_coord(**params)
         self.label.setPixmap(QPixmap.fromImage(QImage.fromData(data)))
 
-    def keyPressEvent(self, event: QKeyEvent) -> None:
+    def keyReleaseEvent(self, event: Optional[QKeyEvent]) -> None:
+        if event.key() == Qt.Key.Key_Up:
+            self.current_pos[1] *= (1.000008 + 0.000005 * (21 - self.zoom))
+        elif event.key() == Qt.Key.Key_Down:
+            self.current_pos[1] /= (1.000008 + 0.000005 * (21 - self.zoom))
+        elif event.key() == Qt.Key.Key_Left:
+            self.current_pos[0] /= (1.000008 + 0.000005 * (21 - self.zoom))
+        elif event.key() == Qt.Key.Key_Right:
+            self.current_pos[0] *= (1.000008 + 0.000005 * (21 - self.zoom))
+        self.set_image()
+
+    def keyPressEvent(self, event: QKeyEvent):
         if event.key() == Qt.Key.Key_PageUp:
             self.zoom = min(21, self.zoom + 1)
         elif event.key() == Qt.Key.Key_PageDown:
