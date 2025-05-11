8b7601f42ba45f2c5b9b55142595d0263dd5d6bc
Dima <d.chernushin@gmail.com>
2025-05-11 17:44:18 +0300

    Homework 2 and 3

--- Work/main.py
+++ Work/main.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from PyQt6.QtCore import pyqtSignal, pyqtBoundSignal, Qt
 from PyQt6.QtGui import QPixmap, QImage, QKeyEvent
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit
 
 from API_KEYS import *
 from Work.UI.Window import Ui_MainWindow
@@ -19,17 +19,40 @@ class SearchMapApp(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setFixedSize(650, 450)
         self.button = QPushButton('Светлая/Тёмная', self)
-        self.button.move(530, 30)
+        self.button.move(10, 410)
+        self.search_area = QLineEdit(self)
+        self.search_area.resize(540, 30)
+        self.search_area.move(10, 10)
+
+        self.search_btn = QPushButton("Найти", self)
+        self.search_btn.move(560, 10)
+        bound: pyqtSignal | pyqtBoundSignal = self.search_btn.clicked
+        bound.connect(self.search)
         bound: pyqtSignal | pyqtBoundSignal = self.button.clicked
         bound.connect(self.set_theme)
 
         self.current_theme = 'light'
-
-        self.setFixedSize(650, 450)
+        self.marked_point: list[tuple[float, float], ...] = list()
         self.zoom = 17
         self.current_pos = None
         self.set_default_position()
+        self.set_image()
+
+    def search(self):
+        request = self.search_area.text()
+        if not request:
+            return
+        params = {
+            'apikey': GEOCODE_API_KEY,
+            'geocode': request,
+            'format': 'json'
+        }
+        coords = get_coord_from_object(get_object(get_geocode_data(**params)))
+        self.current_pos = list(coords)
+        self.marked_point = coords
+        self.search_area.clearFocus()
         self.set_image()
 
     def set_default_position(self):
@@ -52,6 +75,8 @@ class SearchMapApp(QMainWindow, Ui_MainWindow):
             'size': '650,450',
             'theme': self.current_theme
         }
+        if self.marked_point:
+            params['pt'] = ','.join(map(str, self.marked_point)) + ',pm2gnl'
         data = get_image_from_coord(**params)
         self.label.setPixmap(QPixmap.fromImage(QImage.fromData(data)))
 
